90682a6200a07c837bd33c38593467c682c05e6b
digitalankit <akjaph@gmail.com>
2020-12-07 02:16:43 +0530

    using eslint to check for javascript errors

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true
+    },
+    extends: [
+        "eslint:recommended",
+        "plugin:react/recommended",
+        "plugin:@typescript-eslint/recommended",
+        "prettier",
+        "prettier/@typescript-eslint",
+        "prettier/react"
+    ],
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true
+        },
+        ecmaVersion: 12,
+        sourceType: "module"
+    },
+    plugins: ["react", "@typescript-eslint"],
+    settings: {
+        react: {
+            version: "detect"
+        }
+    },
+    rules: {}
+};
