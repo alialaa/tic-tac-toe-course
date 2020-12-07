535c12af0ec8332f2413ffcc960cf49464576853
digitalankit <akjaph@gmail.com>
2020-12-07 20:25:48 +0530

    test branch full code

--- babel.config.js
+++ babel.config.js
@@ -1,20 +1,21 @@
-const { Component } = require("react");
-
 module.exports = function (api) {
     api.cache(true);
     return {
         presets: ["babel-preset-expo"],
         plugins: [
-            ["module-resolver", {
-                alias: {
-                    "@screens": "./src/screens",
-                    "@Components": "./src/components",
-                    "@utils": "./src/utils",
-                    "@contexts": "./src/contexts",
-                    "@config": "./src/config",
-                    "@assets": "./assets",
+            [
+                "module-resolver",
+                {
+                    alias: {
+                        "@screens": "./src/screens",
+                        "@Components": "./src/components",
+                        "@utils": "./src/utils",
+                        "@contexts": "./src/contexts",
+                        "@config": "./src/config",
+                        "@assets": "./assets"
+                    }
                 }
-            }]
+            ]
         ]
     };
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,12 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Image } from "react-native";
 import { Game, Home } from "@screens";
 
 export default function App() {
     return (
         <View style={styles.container}>
+            <Image source={require("../assets/icon.png")} />
             <Home />
             <Game />
         </View>
