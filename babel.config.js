3d4e2abdde5d86179afd89710657f5ea3839b0d8
digitalankit <akjaph@gmail.com>
2020-12-07 14:59:15 +0530

    testing

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,20 @@
+const { Component } = require("react");
+
 module.exports = function (api) {
     api.cache(true);
     return {
-        presets: ["babel-preset-expo"]
+        presets: ["babel-preset-expo"],
+        plugins: [
+            ["module-resolver", {
+                alias: {
+                    "@screens": "./src/screens",
+                    "@Components": "./src/components",
+                    "@utils": "./src/utils",
+                    "@contexts": "./src/contexts",
+                    "@config": "./src/config",
+                    "@assets": "./assets",
+                }
+            }]
+        ]
     };
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
-import { Game, Home } from "./screens";
+import { Game, Home } from "@screens";
 
 export default function App() {
     return (
