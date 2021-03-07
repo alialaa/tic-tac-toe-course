3177f17b46e13805ee5c8ef2fbc35b7ceba6480e
digitalankit <akjaph@gmail.com>
2021-03-07 15:52:24 +0530

    creating-a-custom-textinput-component-with-ref-forwarding

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,3 +3,4 @@ export { default as AppBootstrap } from "./app-bootstrap/app-bootstrap";
 export { default as GradientBackground } from "./gradient-background/gradient-background";
 export { default as Button } from "./button/button";
 export { default as Board } from "./board/board";
+export { default as TextInput } from "./text-input/text-input";
--- src/screens/login/login.tsx
+++ src/screens/login/login.tsx
@@ -1,26 +1,26 @@
-import React, { ReactElement } from "react";
-import { ScrollView, TextInput } from "react-native";
-import { GradientBackground } from "@Components";
+import React, { ReactElement, useRef } from "react";
+import { ScrollView, TextInput as NativeTextInput } from "react-native";
+import { GradientBackground, TextInput } from "@Components";
 import styles from "./login.styles";
-import { colors } from "@utils";
 
 export default function Login(): ReactElement {
+    const passwordRef = useRef<NativeTextInput | null>(null);
     return (
         <GradientBackground>
             <ScrollView contentContainerStyle={styles.container}>
                 <TextInput
+                    returnKeyType="next"
                     placeholder="Username"
-                    placeholderTextColor="#5d5379"
-                    style={{
-                        height: 50,
-                        width: "100%",
-                        borderBottomWidth: 1,
-                        borderColor: colors.lightGreen,
-                        backgroundColor: colors.purple,
-                        padding: 10,
-                        color: colors.lightGreen,
-                        fontFamily: "DeliusUnicase_400Regular"
+                    style={{ marginBottom: 20 }}
+                    onSubmitEditing={() => {
+                        passwordRef.current?.focus();
                     }}
+                />
+                <TextInput
+                    ref={passwordRef}
+                    returnKeyType="done"
+                    secureTextEntry
+                    placeholder="Password"
                 />
             </ScrollView>
         </GradientBackground>
