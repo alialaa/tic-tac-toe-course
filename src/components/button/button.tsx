79db54cabe88902117cc332770a765069df222fe
digitalankit <akjaph@gmail.com>
2021-03-07 21:17:46 +0530

    code fix

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,21 +1,16 @@
 import React, { ReactElement } from "react";
-import { TouchableOpacity, TouchableOpacityProps, ActivityIndicator } from "react-native";
+import { TouchableOpacity, TouchableOpacityProps } from "react-native";
 import styles from "./button.styles";
 import Text from "../text/text";
 
 type ButtonProps = {
     title: string;
-    loading?: boolean;
 } & TouchableOpacityProps;
 
-export default function Button({ title, style, loading, ...props }: ButtonProps): ReactElement {
+export default function Button({ title, style, ...props }: ButtonProps): ReactElement {
     return (
-        <TouchableOpacity disabled={loading} {...props} style={[styles.button, style]}>
-            {loading ? (
-                <ActivityIndicator color="#000" />
-            ) : (
-                <Text style={styles.buttonText}>{title}</Text>
-            )}
+        <TouchableOpacity {...props} style={[styles.button, style]}>
+            <Text style={styles.buttonText}>{title}</Text>
         </TouchableOpacity>
     );
 }
