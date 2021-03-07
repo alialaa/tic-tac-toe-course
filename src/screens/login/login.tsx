cd9c8fa47c6ce971ba28cbae743b52445d54685a
digitalankit <akjaph@gmail.com>
2021-03-07 07:31:24 +0530

    adding-and-styling-text-inputs-in-the-login-screen

--- src/screens/login/login.tsx
+++ src/screens/login/login.tsx
@@ -1,10 +1,28 @@
 import React, { ReactElement } from "react";
-import { View, Text } from "react-native";
+import { ScrollView, TextInput } from "react-native";
+import { GradientBackground } from "@Components";
+import styles from "./login.styles";
+import { colors } from "@utils";
 
 export default function Login(): ReactElement {
     return (
-        <View>
-            <Text>Login</Text>
-        </View>
+        <GradientBackground>
+            <ScrollView contentContainerStyle={styles.container}>
+                <TextInput
+                    placeholder="Username"
+                    placeholderTextColor="#5d5379"
+                    style={{
+                        height: 50,
+                        width: "100%",
+                        borderBottomWidth: 1,
+                        borderColor: colors.lightGreen,
+                        backgroundColor: colors.purple,
+                        padding: 10,
+                        color: colors.lightGreen,
+                        fontFamily: "DeliusUnicase_400Regular"
+                    }}
+                />
+            </ScrollView>
+        </GradientBackground>
     );
 }
