e2ed16b2a1235388a997e8a8b3eb56c1b5fd95d2
digitalankit <akjaph@gmail.com>
2021-03-07 22:29:32 +0530

    creating-a-react-context-to-store-the-logged-in-user

--- src/components/app-bootstrap/app-bootstrap.tsx
+++ src/components/app-bootstrap/app-bootstrap.tsx
@@ -1,4 +1,12 @@
-import React, { ReactElement, ReactNode, useState, useEffect } from "react";
+import React, {
+    ReactElement,
+    ReactNode,
+    useState,
+    useEffect,
+    createContext,
+    Dispatch,
+    SetStateAction
+} from "react";
 import {
     useFonts,
     DeliusUnicase_400Regular,
@@ -11,25 +19,44 @@ type AppBootstrapProps = {
     children: ReactNode;
 };
 
+type AuthContextType = {
+    user: { [key: string]: any } | null;
+    setUser: Dispatch<SetStateAction<{ [key: string]: any } | null>>;
+};
+
+const AuthContext = createContext<AuthContextType | undefined>(undefined);
+
 export default function AppBootstrap({ children }: AppBootstrapProps): ReactElement {
     const [fontLoaded] = useFonts({
         DeliusUnicase_400Regular,
         DeliusUnicase_700Bold
     });
     const [authLoaded, setAuthLoaded] = useState(false);
+    const [user, setUser] = useState<{ [key: string]: any } | null>(null);
 
     useEffect(() => {
         async function checkCurrentUser() {
             try {
                 const user = await Auth.currentAuthenticatedUser();
-                console.log(user);
+                setUser(user);
             } catch (error) {
-                console.log(error);
+                setUser(null);
             }
             setAuthLoaded(true);
         }
 
         checkCurrentUser();
     }, []);
-    return fontLoaded && authLoaded ? <>{children}</> : <AppLoading />;
+    return fontLoaded && authLoaded ? (
+        <AuthContext.Provider
+            value={{
+                user,
+                setUser
+            }}
+        >
+            {children}
+        </AuthContext.Provider>
+    ) : (
+        <AppLoading />
+    );
 }
