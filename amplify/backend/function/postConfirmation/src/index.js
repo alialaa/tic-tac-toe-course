2689523e65d938175c947d27558dde5b959bdcf1
digitalankit <akjaph@gmail.com>
2021-04-11 19:53:04 +0530

    writing-the-post-confirmation-lambda-function

--- amplify/backend/function/postConfirmation/src/index.js
+++ amplify/backend/function/postConfirmation/src/index.js
@@ -6,12 +6,51 @@
 	REGION
 Amplify Params - DO NOT EDIT */
 
-const appsync = require("aws-amplify");
-const ggl = require("graphql-tag");
+const appsync = require("aws-appsync");
+const gql = require("graphql-tag");
 require("cross-fetch/polyfill");
 
 exports.handler = async (event, context, callback) => {
     const graphqlClient = new appsync.AWSAppSyncClient({
-        url: 
-    })
+        url: process.env.API_TICTACTOE_GRAPHQLAPIENDPOINTOUTPUT,
+        region: process.env.REGION,
+        auth: {
+            type: "AWS_IAM",
+            Credentials: {
+                accessKeyID: process.env.AWS_ACCESS_KEY_ID,
+                secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+                sessionToken: process.env.AWS_SESSION_TOKEN
+            }
+        },
+        disableOffline: true
+    });
+
+    const mutation = gql`
+        mutation createPlayer(
+            $name: String!
+            $cognitoID: String!
+            $username: String!
+            $email: AWSEmail!
+        ) {
+            createPlayer(
+                input: { cognitoID: $cognitoID, email: $email, name: $name, username: $username }
+            ) {
+                id
+            }
+        }
+    `;
+    try {
+        await graphqlClient.mutate({
+            mutation,
+            variables: {
+                name: event.request.userAttributes.name,
+                username: event.username,
+                cognitoID: event.request.userAttributes.sub,
+                email: event.request.userAttributes.email
+            }
+        });
+        callback(null, event);
+    } catch (error) {
+        callback(error);
+    }
 };
