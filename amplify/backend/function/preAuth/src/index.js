5e75aef860fac5419c199a895eff3775832bc4a5
Rajdeep Ratan <rajdeepratan@hotmail.com>
2021-05-07 16:35:12 -0400

    Amplify pull update

--- amplify/backend/function/preAuth/src/index.js
+++ amplify/backend/function/preAuth/src/index.js
@@ -23,6 +23,7 @@ exports.handler = async (event, context, callback) => {
         },
         disableOffline: true
     });
+    console.log("Hello World");
 
     const query = gql`
         query getPlayer($username: String!) {
